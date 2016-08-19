fccc669eadca982e0b4e526106c989f0a3b45ad3
Dusan Klinec <dusan.klinec@gmail.com>
2016-08-19 11:15:14 +0200

    javadoc added

--- src/main/java/com/enigmabridge/retry/EBFuture.java
+++ src/main/java/com/enigmabridge/retry/EBFuture.java
@@ -1,11 +1,36 @@
 package com.enigmabridge.retry;
 
 /**
+ * Future object, similar to {@link java.util.concurrent.Future}
+ *
+ * Used in async job invocations. Can be used to detect if task is running or
+ * finished. Caller can cancel the task or skip current backoff waiting interval.
+ *
  * Created by dusanklinec on 21.07.16.
  */
 public interface EBFuture<Result, Error> {
+    /**
+     * Returns true if async task is still running.
+     * @return true if running
+     */
     boolean isRunning();
+
+    /**
+     * Returns true if async task finished its execution.
+     * @return true if done
+     */
     boolean isDone();
+
+    /**
+     * Triggers cancellation of the task.
+     * Does not interrupt currently running job, disables invocation of the next attempt.
+     */
     void cancel();
+
+    /**
+     * If task is currently waiting in a backoff interval, this interrupts waiting and
+     * immediately executes the task - skipping the waiting interval.
+     * Like "Retry now" button in UI.
+     */
     void runNow();
 }
--- src/main/java/com/enigmabridge/retry/EBRetryJobSimpleSafeThrErr.java
+++ src/main/java/com/enigmabridge/retry/EBRetryJobSimpleSafeThrErr.java
@@ -1,6 +1,10 @@
 package com.enigmabridge.retry;
 
 /**
+ * Retry Job implementation with job error = Throwable.
+ *
+ * Moreover all uncaught exceptions thrown in the job body are caught and considered as fail().
+ *
  * Created by dusanklinec on 25.07.16.
  */
 public abstract class EBRetryJobSimpleSafeThrErr<Result> extends EBRetryJobSimpleSafe<Result, Throwable> {
